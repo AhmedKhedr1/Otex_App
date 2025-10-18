bf9e09babfe36cf3dbb45cbe27ee49b04d9b239d
ahmedkhedr <ahmedkhedrx1@gmail.com>
2025-10-18 14:05:44 +0300

    Refactor ViewsBoostCard layout; enhance visual structure with a new Stack widget for number display and update text styles

--- lib/Features/Profile/Presentation/Views/Widgets/ViewsBoostCard .dart
+++ lib/Features/Profile/Presentation/Views/Widgets/ViewsBoostCard .dart
@@ -3,7 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:otex_app/Core/utils/Text_Styless.dart';
 
 class ViewsBoostCard extends StatelessWidget {
-  final String number; // الرقم فقط
+  final String number;
 
   const ViewsBoostCard({super.key, required this.number});
 
@@ -13,70 +13,51 @@ class ViewsBoostCard extends StatelessWidget {
       width: 71.w,
       height: 74.h,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            number,
-            style: TextStyless.tajawalBold14.copyWith(color: Color(0xff3A813F)),
+          SizedBox(
+            width: 60.w,
+            height: 30.h,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Positioned(
+                  top: 0,
+                  child: Container(
+                    width: 60.w,
+                    height: 30.h,
+                    decoration: BoxDecoration(
+                      color: Color(0xffF5F8F5),
+                      border: Border(
+                        top: BorderSide(color: Color(0xff3A813F), width: 2),
+                      ),
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(90),
+                        topRight: Radius.circular(90),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 6.h,
+                  child: Text(
+                    number,
+                    style: TextStyless.tajawalBold14.copyWith(
+                      color: Color(0xff3A813F),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-          SizedBox(height: 4.h),
+          SizedBox(height: 6.h),
           Text(
             'ضعف عدد المشاهدات',
             textAlign: TextAlign.center,
-            style: TextStyless.tajawalRegular12.copyWith(
-              color: Color(0xff090F1F),
-            ),
+            style: TextStyless.tajawalRegular12,
           ),
         ],
       ),
     );
   }
 }
-      // width: 71.w,
-      // height: 74.h,
-
-      // child: Column(
-      //   mainAxisAlignment: MainAxisAlignment.center,
-      //   children: [
-      //     SizedBox(
-      //       width: 60.w,
-      //       height: 30.h,
-      //       child: Stack(
-      //         alignment: Alignment.center,
-      //         children: [
-      //           Positioned(
-      //             top: 0,
-      //             child: Container(
-      //               width: 60.w,
-      //               height: 30.h,
-      //               decoration: BoxDecoration(
-      //                 border: Border(
-      //                   top: BorderSide(color: Color(0xff3A813F), width: 2),
-      //                 ),
-      //                 borderRadius: const BorderRadius.only(
-      //                   topLeft: Radius.circular(32),
-      //                   topRight: Radius.circular(32),
-      //                 ),
-      //               ),
-      //             ),
-      //           ),
-      //           // الرقم
-      //           Positioned(
-      //             top: 6.h,
-      //             child: Text(
-      //               number,
-      //               style: TextStyless.tajawalBold14.copyWith(
-      //                 color: Color(0xff3A813F),
-      //               ),
-      //             ),
-      //           ),
-      //         ],
-      //       ),
-      //     ),
-      //     SizedBox(height: 6.h),
-      //     Text(
-      //       'ضعف عدد المشاهدات',
-      //       textAlign: TextAlign.center,
-      //       style: TextStyless.tajawalRegular12,
-      //     ),
-      //   ],
-      // ),
